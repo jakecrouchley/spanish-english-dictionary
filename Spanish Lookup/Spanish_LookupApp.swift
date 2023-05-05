ecf3bb7e818d2f8516258ad8f83a875e8e014690
Jake Crouchley <jakecrouchley@gmail.com>
2023-05-05 16:55:59 +0200

    Initial Commit

---
+++ Spanish Lookup/Spanish_LookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spanish_LookupApp.swift
+//  Spanish Lookup
+//
+//  Created by Jake Crouchley on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Spanish_LookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
