ae02cd967f6f14ec5606316fe2a6cc48e22cbd69
Jake Crouchley <jakecrouchley@gmail.com>
2023-05-25 10:21:39 +0200

    Revert "Filling out WordFetchController to handle NSFetchRequests and adding FilteredWordList to investigate searching and manual lazy loading"
    This reverts commit 4593fbb987c6a4846be873f28313efcf76801ad6.

--- Spanish Lookup/ContentView.swift
+++ Spanish Lookup/ContentView.swift
@@ -9,22 +9,61 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @SectionedFetchRequest
+    private var words: SectionedFetchResults<String?, Word>
+
     @Environment(\.managedObjectContext) var managedObjectContext: NSManagedObjectContext
     
+    @State private var searchTerm: String = ""
     @FocusState private var searchFieldIsFocused: Bool
     
-    @State private var searchQuery: String = ""
-    @StateObject var wordFetchController = WordFetchController.shared
+    let nsSortDescriptors = [NSSortDescriptor(key: "first_char", ascending: true), NSSortDescriptor(key: "source_word", ascending: true)]
+    
+    var searchQuery: Binding<String> {
+        Binding {
+            searchTerm
+        } set: { newValue in
+            searchTerm = newValue
+
+            guard !newValue.isEmpty else {
+                let config = words
+                config.nsPredicate = nil
+                config.nsSortDescriptors = nsSortDescriptors
+                return
+            }
+            
+            let config = words
+            config.nsPredicate = NSPredicate(
+            format: "source_word contains[cd] %@",
+            newValue)
+            config.sortDescriptors = [SortDescriptor(\.source_word, order: .reverse)]
+        }
+    }
+    
+    init() {
+        let request: NSFetchRequest<Word> = Word.fetchRequest()
+        request.sortDescriptors = nsSortDescriptors
+        request.fetchLimit = 10
+        _words = SectionedFetchRequest(fetchRequest: request, sectionIdentifier: \.first_char)
+    }
+
 
     var body: some View {
         NavigationStack {
-            FilteredWordList()
-        }.searchable(text: $wordFetchController.searchText, placement: .navigationBarDrawer(displayMode: .always))
+            List(words) { section in
+                Section(header: Text(section.id ?? "")) {
+                    ForEach(section) {word in
+                        WordRow(word: word).onAppear(perform: {
+                            print("\(word.source_word ?? "") appeared")
+                        })
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .padding(.zero)
+        }.searchable(text: searchQuery, placement: .navigationBarDrawer(displayMode: .always))
             .textInputAutocapitalization(.never)
             .padding(.zero)
-            .onChange(of: wordFetchController.searchText) { newValue in
-                wordFetchController.performFetch()
-            }
             
     }
 }
